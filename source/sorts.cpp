528d26ac7ee252a5878d5c9da60cb490b10be220
Kyle <kyle_allhusen@my.uri.edu>
2019-06-04 18:28:46 -0400

    Bubble Sort Try 2

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "sorts.hpp"
 
+//O(n^2) - Omega(n)
+//
 void bubble_sort(int *arr, int n) {
     int temp = 0;
     for(int i = 0; i < (n - 1); i++) {
@@ -8,10 +10,9 @@ void bubble_sort(int *arr, int n) {
             temp = arr[i];
             arr[i] = arr[i + 1];
             arr[i + 1] = temp;
-            bubble_sort(arr, n);
         }
     }
     if (temp != 0) {
-        bubble_sort(arr, n);
+        bubble_sort(arr, n - 1);
     }
 }
