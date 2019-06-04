3685a8836be7ddb7ffc11156b757dbcb5a32b235
Kyle <kyle_allhusen@my.uri.edu>
2019-06-04 18:27:27 -0400

    Bubble Sort Try 1

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -1,2 +1,17 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "sorts.hpp"
+
+void bubble_sort(int *arr, int n) {
+    int temp = 0;
+    for(int i = 0; i < (n - 1); i++) {
+        if(arr[i] > arr[i + 1]){
+            temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            bubble_sort(arr, n);
+        }
+    }
+    if (temp != 0) {
+        bubble_sort(arr, n);
+    }
+}
