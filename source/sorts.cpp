6d276c1d267e8b9d00a8152bbb74df4208926e67
Kyle <kyle_allhusen@my.uri.edu>
2019-06-04 18:42:56 -0400

    Bubble Sort Try 3

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -4,15 +4,14 @@
 //O(n^2) - Omega(n)
 //
 void bubble_sort(int *arr, int n) {
-    int temp = 0;
     for(int i = 0; i < (n - 1); i++) {
-        if(arr[i] > arr[i + 1]){
-            temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
+        if(arr[i] > arr[i + 1]) {
+            arr[i] = arr[i] xor arr[i + 1];
+            arr[i + 1] = arr[i] xor arr[i + 1];
+            arr[i] = arr[i] xor arr[i + 1];
         }
     }
-    if (temp != 0) {
+    if ( n != 1) {
         bubble_sort(arr, n - 1);
     }
 }
