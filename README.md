6e75ef00f1929b99e4454de5ef6d1276f153514b
Tom <thoward12@outlook.com>
2019-06-05 13:32:43 -0400

    Preparing for today's class

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Welcome to CSC 212, Data Structures and Algorithms!
 - [FAQ](docs/FAQ.md)
 - [Github Handbook](docs/github.md)
 - [IDE Guide](docs/IDE.md)
-- [Crash Course C++](docs/CrashCourseCpp.pdf)
+- [Crash Course C++](docs/notes/CrashCourseCpp.pdf)
 - [Slack Room](https://csc212.slack.com/)
 - [DevDocs](https://devdocs.io/)
 - [Cheat Sheets](docs/cheat_sheets/)
@@ -24,10 +24,14 @@ Welcome to CSC 212, Data Structures and Algorithms!
 
 ## Readings
 
+- [Git](https://git-scm.com/)
+  - [Basics](https://git-scm.com/book/en/v1/Getting-Started-Git-Basics)
+- [Github](https://github.com/)
+  - [Handbook](https://guides.github.com/introduction/git-handbook/)
 - [OpenDSA](https://opendsa-server.cs.vt.edu/ODSA/Books/Everything/html/)
   - Chapters: 4, 6, 8, 10, [13.1, 13.7], [13.9, 13.11], [13.15, 13.17]
 - [Accelerated C++](https://www.google.com/search?q=accelerated+c%2B%2B&oq=accelerated+c%2B%2B&aqs=chrome..69i57j69i60j0l4.2026j0j7&client=ubuntu&sourceid=chrome&ie=UTF-8)
-  - Chapters: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1
+  - Chapters: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 - [VS Code: Getting Started](https://code.visualstudio.com/docs/getstarted/introvideos)
   - Watch: Introductory Videos (all)
   - Read: Tips and Tricks, User Interface, Themes, and Settings
--- source/sorts.hpp
+++ source/sorts.hpp
@@ -3,13 +3,18 @@
 
 #include "doctest/doctest/doctest.h"
 
-/* Sorting Algorithms */
-
 typedef void (*sort_fn)(int*, int);
 
 void bubble_sort(int* arr, int n);
 
-TEST_CASE("Testing sorting functions") {
+void insertion_sort(int* arr, int n);
+
+void selection_sort(int* arr, int n);
+
+sort_fn sorts[] = {bubble_sort, insertion_sort, selection_sort};
+char names[][20] = {"Bubble sort", "Insertion sort", "Selection sort"};
+
+TEST_CASE("Sorting Functions.") {
   int arr1[1] = {1};
   int arr2[2] = {2, 1};
   int arr3[5] = {5, 4, 3, 2, 1};
@@ -18,27 +23,29 @@ TEST_CASE("Testing sorting functions") {
   int arr5[10] = {3, 1, 3, 2, 1, 3, 2, 2, 1, 2};
   int arr5_sorted[10] = {1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
 
-  SUBCASE("Bubble") {
-    bubble_sort(arr1, 1);
-    CHECK(arr1[0] == 1);
+  for (int i = 0; i < sizeof(sorts) / sizeof(sort_fn); ++i) {
+    SUBCASE(names[i]) {
+      sorts[i](arr1, 1);
+      CHECK(arr1[0] == 1);
 
-    bubble_sort(arr2, 2);
-    CHECK(arr2[0] == 1);
-    CHECK(arr2[1] == 2);
+      sorts[i](arr2, 2);
+      CHECK(arr2[0] == 1);
+      CHECK(arr2[1] == 2);
 
-    bubble_sort(arr3, 5);
-    for (int j = 0; j < 5; ++j) {
-      CHECK(arr3[j] == j + 1);
-    }
+      sorts[i](arr3, 5);
+      for (int j = 0; j < 5; ++j) {
+        CHECK(arr3[j] == j + 1);
+      }
 
-    bubble_sort(arr4, 10);
-    for (int j = 0; j < 10; ++j) {
-      CHECK(arr4[j] == arr4_sorted[j]);
-    }
+      sorts[i](arr4, 10);
+      for (int j = 0; j < 10; ++j) {
+        CHECK(arr4[j] == arr4_sorted[j]);
+      }
 
-    bubble_sort(arr5, 10);
-    for (int j = 0; j < 10; ++j) {
-      CHECK(arr5[j] == arr5_sorted[j]);
+      sorts[i](arr5, 10);
+      for (int j = 0; j < 10; ++j) {
+        CHECK(arr5[j] == arr5_sorted[j]);
+      }
     }
   }
 }
