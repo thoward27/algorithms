e624e3facc247f4cefecbd32d67ae3680296a03a
Tom <thoward12@outlook.com>
2019-06-05 13:34:04 -0400

    Updated source file

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -1,17 +1,26 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "sorts.hpp"
 
-//O(n^2) - Omega(n)
-//
-void bubble_sort(int *arr, int n) {
-    for(int i = 0; i < (n - 1); i++) {
-        if(arr[i] > arr[i + 1]) {
-            arr[i] = arr[i] xor arr[i + 1];
-            arr[i + 1] = arr[i] xor arr[i + 1];
-            arr[i] = arr[i] xor arr[i + 1];
-        }
+/** Bubble Sort
+ *  O(n^2), Omega(n)
+ */
+void bubble_sort(int* arr, int n) {
+  for (int i = 0; i < (n - 1); i++) {
+    if (arr[i] > arr[i + 1]) {
+      arr[i] = arr[i] xor arr[i + 1];
+      arr[i + 1] = arr[i] xor arr[i + 1];
+      arr[i] = arr[i] xor arr[i + 1];
     }
-    if ( n != 1) {
-        bubble_sort(arr, n - 1);
-    }
+  }
+  if (n != 1) {
+    bubble_sort(arr, n - 1);
+  }
 }
+
+/** Selection Sort
+ * O(?), Omega(?), Theta(?)
+ */
+
+/** Insertion Sort
+ * O(?), Omega(?), Theta(?)
+ */
