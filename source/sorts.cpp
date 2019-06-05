1a0c3699f82ef46833be41fafc6611cbbe4adb97
camrenwoodstock <camrenwoodstock@gmail.com>
2019-06-05 16:39:52 -0400

    Sorting Lab Done!

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -17,6 +17,46 @@ void bubble_sort(int* arr, int n) {
   }
 }
 
+
+void swap(int * y, int * x) {
+  int temp;
+  temp = *y;
+  *y = *x;
+  *x = temp;
+}
+
+void selection_sort(int* arr, int n) {
+ 
+  for(int i = 0; i < (n-1); i++) {
+    int min = i;
+    for (int j = i + 1; j < n ; j++) {
+        if(arr[j] < arr[min]) min = j;
+    }
+    swap(&arr[min],&arr[i]);
+    
+  }
+  
+}
+
+void insertion_sort(int* arr, int n) {
+    for(int i = 0; i < n; i++) {
+      // Set j = i 
+      // j > 0 -> for each j from i to 0
+      // if the element at j-1 is greater than the element at j 
+      // -> arr[j] < arr[j-1]
+      for(int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+        //then swap the elements
+        swap(&arr[j],&arr[j-1]);
+      }
+    }
+
+}
+
+
+
+
+
+
 /** Selection Sort
  * O(?), Omega(?), Theta(?)
  */
