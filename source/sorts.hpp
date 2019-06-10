3207b6f3e19cba416824eebc7779a45a0bd98dc0
Tom <thoward12@outlook.com>
2019-06-10 09:23:31 -0400

    added quicksort to tests

--- source/sorts.hpp
+++ source/sorts.hpp
@@ -1,5 +1,4 @@
-#ifndef __SORTS__
-#define __SORTS__
+#pragma once
 #include "doctest/doctest/doctest.h"
 
 /* Auxiliary Functions */
@@ -46,6 +45,50 @@ TEST_CASE("Testing merge function") {
   }
 }
 
+int partition(int* arr, int lo, int hi);
+
+TEST_CASE("Testing partition function") {
+  int arr1[1] = {1};
+  int arr2[2] = {2, 1};
+  int arr3[5] = {5, 4, 3, 2, 1};
+  int arr4[10] = {23, 41, 54, 64, 92, 28, 35, 40, 81, 82};
+  int arr5[10] = {1, 1, 2, 3, 3, 1, 2, 2, 2, 3};
+
+  int p1 = partition(arr1, 0, 0);
+  CHECK(p1 == 0);
+  CHECK(arr1[0] == 1);
+
+  partition(arr2, 0, 1);
+  CHECK(arr2[0] == 1);
+  CHECK(arr2[1] == 2);
+
+  int p3 = partition(arr3, 0, 4);
+  for (int i = 0; i < p3; ++i) {
+    CHECK(arr3[i] <= arr3[p3]);
+  }
+  for (int i = p3 + 1; i < 5; ++i) {
+    CHECK(arr3[i] >= arr3[p3]);
+  }
+
+  int p4 = partition(arr4, 0, 9);
+  for (int i = 0; i < p4; ++i) {
+    CHECK(arr4[i] <= arr4[p4]);
+  }
+  for (int i = p4 + 1; i < 10; ++i) {
+    CHECK(arr4[i] >= arr4[p4]);
+  }
+
+  int p5 = partition(arr5, 0, 9);
+  for (int i = 0; i < p5; ++i) {
+    CHECK(arr5[i] <= arr5[p5]);
+  }
+  for (int i = p5 + 1; i < 10; ++i) {
+    CHECK(arr5[i] >= arr5[p5]);
+  }
+}
+
+/* Sorting Algorithms */
+
 typedef void (*sort_fn)(int*, int);
 
 void bubble_sort(int* arr, int n);
@@ -56,9 +99,14 @@ void selection_sort(int* arr, int n);
 
 void mergesort(int* arr, int n);
 
-sort_fn sorts[] = {bubble_sort, insertion_sort, selection_sort, mergesort};
+void quicksort(int* arr, int n);
+
+void quicksort(int* arr, int lo, int hi);
+
+sort_fn sorts[] = {bubble_sort, insertion_sort, selection_sort, mergesort,
+                   quicksort};
 char names[][20] = {"Bubble sort", "Insertion sort", "Selection sort",
-                    "Mergesort"};
+                    "Mergesort", "Quicksort"};
 
 TEST_CASE("Testing sorting functions") {
   int arr1[1] = {1};
@@ -69,7 +117,7 @@ TEST_CASE("Testing sorting functions") {
   int arr5[10] = {3, 1, 3, 2, 1, 3, 2, 2, 1, 2};
   int arr5_sorted[10] = {1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
 
-  for (int i = 0; i < sizeof(sorts) / sizeof(sort_fn); ++i) {
+  for (int i = 0; i < 5; ++i) {
     SUBCASE(names[i]) {
       sorts[i](arr1, 1);
       CHECK(arr1[0] == 1);
@@ -95,5 +143,3 @@ TEST_CASE("Testing sorting functions") {
     }
   }
 }
-
-#endif
