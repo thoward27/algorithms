660efc0a779586860ea41657a62855ec5b0f3220
Tom <thoward12@outlook.com>
2019-06-10 09:22:31 -0400

    Cleaned up merged in changes

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -1,6 +1,21 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "sorts.hpp"
 
+/* Helper Functions */
+
+void copy_arr(int* arr, int* temp, int n) {
+  for (int i = 0; i < n; i++) {
+    temp[i] = arr[i];
+  }
+}
+
+void swap(int* y, int* x) {
+  int temp;
+  temp = *y;
+  *y = *x;
+  *x = temp;
+}
+
 /** Bubble Sort
  *  O(n^2), Omega(n)
  */
@@ -17,80 +32,53 @@ void bubble_sort(int* arr, int n) {
   }
 }
 
-
-void swap(int * y, int * x) {
-  int temp;
-  temp = *y;
-  *y = *x;
-  *x = temp;
-}
-
 void selection_sort(int* arr, int n) {
- 
-  for(int i = 0; i < (n-1); i++) {
+  for (int i = 0; i < (n - 1); i++) {
     int min = i;
-    for (int j = i + 1; j < n ; j++) {
-        if(arr[j] < arr[min]) min = j;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min])
+        min = j;
     }
-    swap(&arr[min],&arr[i]);
-    
+    swap(&arr[min], &arr[i]);
   }
 }
 
 void insertion_sort(int* arr, int n) {
-    for(int i = 0; i < n; i++) {
-      // Set j = i 
-      // j > 0 -> for each j from i to 0
-      // if the element at j-1 is greater than the element at j 
-      // -> arr[j] < arr[j-1]
-      for(int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
-        //then swap the elements
-        swap(&arr[j],&arr[j-1]);
-      }
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+      swap(&arr[j], &arr[j - 1]);
     }
-
-}
-
-void copy_arr(int *arr, int *temp, int n) {
-  for(int i = 0; i < n; i++) {
-    temp[i] = arr[i];
   }
 }
 
-void merge(int *arr, int n, int mid) {
-  int *temp = new int[n];
+void merge(int* arr, int n, int mid) {
+  int* temp = new int[n];
   copy_arr(arr, temp, n);
 
-  int j = 0;int k = mid;
-  for(int i = 0; i < n; i++) {
-    if(j >= mid) {
+  int j = 0;
+  int k = mid;
+  for (int i = 0; i < n; i++) {
+    if (j >= mid) {
       arr[i] = temp[k];
       k++;
-    }
-    else if(k >= n) {
+    } else if (k >= n) {
       arr[i] = temp[j];
       j++;
-    }
-    else if(temp[j] < temp[k]) {
+    } else if (temp[j] < temp[k]) {
       arr[i] = temp[j];
       j++;
-    }
-    else{
+    } else {
       arr[i] = temp[k];
       k++;
     }
   }
+  delete[] temp;
 }
 
-void mergesort(int *arr, int n) {
-  if(n > 1) {
-    mergesort(arr, n /2);
-    mergesort(arr + (n/2), n - n/2);
-    merge(arr, n, n/2);
+void mergesort(int* arr, int n) {
+  if (n > 1) {
+    mergesort(arr, n / 2);
+    mergesort(arr + (n / 2), n - n / 2);
+    merge(arr, n, n / 2);
   }
 }
-
-
-
-
- 
