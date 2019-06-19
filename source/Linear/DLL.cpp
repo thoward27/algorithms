c044582b1cbcdf281ee54b8402a823192282e85d
Tom <thoward12@outlook.com>
2019-06-19 19:30:00 -0400

    Updating from staging

---
+++ source/Linear/DLL.cpp
@@ -0,0 +1,244 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "DLL.hpp"
+
+DLL::DLL() {
+  head = tail = nullptr;
+  len = 0;
+}
+
+DLL::~DLL() {
+  clear();
+}
+
+unsigned int DLL::length() {
+  return len;
+}
+
+void DLL::push_front(int d) {
+  Node* n = new Node(d);
+  if (!head) {
+    head = tail = n;
+  } else {
+    n->next = head;
+    head->prev = n;
+    head = n;
+  }
+  ++len;
+}
+
+int DLL::pop_front() {
+  Node* to_remove = head;
+  int rval = head->data;
+  if (len == 1) {
+    head = tail = nullptr;
+  } else {
+    head->next->prev = nullptr;
+    head = to_remove->next;
+  }
+  delete to_remove;
+  --len;
+  return rval;
+}
+
+int DLL::index(int data) {
+  Node* temp = head;
+  int i = 0;
+  while (temp) {
+    if (temp->data == data)
+      return i;
+    i += 1;
+    temp = temp->next;
+  }
+  return -1;
+}
+
+void DLL::push_back(int d) {
+  Node* n = new Node(d);
+  if (!tail) {
+    head = tail = n;
+  } else {
+    n->prev = tail;
+    tail->next = n;
+    tail = n;
+  }
+  ++len;
+}
+
+int DLL::pop_back() {
+  Node* to_remove = tail;
+  int rval = tail->data;
+  if (len == 1) {
+    head = tail = nullptr;
+  } else {
+    tail->prev->next = nullptr;
+    tail = to_remove->prev;
+  }
+  delete to_remove;
+  --len;
+  return rval;
+}
+
+int DLL::at(int idx) {
+  // Handle negative indexes by making it positive.
+  idx = (idx < 0) ? len + idx : idx;
+
+  // Check for out of bounds.
+  if (idx < 0 || (unsigned int)idx >= len)
+    throw "Bad index";
+
+  // Iterate to the element from the head or tail, whichever is closer.
+  Node* iter;
+  if ((unsigned int)idx < len/2) {
+    iter = head;
+    while (--idx >= 0) {
+      iter = iter->next;
+    }
+  } else {
+    iter = tail;
+    while (++idx < (int)len) {
+      iter = iter->prev;
+    }
+  }
+
+  // Return its data.
+  return iter->data;
+}
+
+int DLL::set(int idx, int d) {
+  // Handle negative indexes by making it positive.
+  idx = (idx < 0) ? len + idx : idx;
+
+  // Check for out of bounds.
+  if (idx < 0 || (unsigned int)idx >= len)
+    throw "Bad index";
+
+  Node* iter;
+  if ((unsigned int)idx < len/2) {
+    iter = head;
+    while (--idx >= 0) {
+      iter = iter->next;
+    }
+  } else {
+    iter = tail;
+    while (++idx < (int)len) {
+      iter = iter->prev;
+    }
+  }
+
+  int old = iter->data;
+  iter->data = d;
+  return old;
+}
+
+void DLL::push(int d, int idx) {
+  if (!idx) {
+    push_front(d);
+  } else if ((unsigned int)idx == len) {
+    push_back(d);
+  } else {
+    Node* n = new Node(d);
+    Node* iter;
+    if ((unsigned)idx < len/2) {
+      iter = head;
+      while (--idx > 0) {
+        iter = iter->next;
+      }
+    } else {
+      iter = tail;
+      while (++idx <= (int)len) {
+        iter = iter->prev;
+      }
+    }
+    n->next = iter->next;
+    n->prev = iter;
+    iter->next = n;
+    n->next->prev = n;
+    ++len;
+  }
+}
+
+int DLL::pop(int idx) {
+  if (!idx) {
+    return pop_front();
+  } else if ((unsigned int)idx == len - 1) {
+    return pop_back();
+  } else {
+    Node* iter;
+    if ((unsigned)idx < len/2) {
+      iter = head;
+      while (--idx > 0) {
+        iter = iter->next;
+      }
+    } else {
+      iter = tail;
+      while (++idx <= (int)len) {
+        iter = iter->prev;
+      }
+    }
+    Node* to_remove = iter->next;
+    iter->next = to_remove->next;
+    iter->next->prev = iter;
+    int ret = to_remove->data;
+    delete to_remove;
+    --len;
+    return ret;
+  }
+}
+
+void DLL::clear() {
+  Node* to_remove;
+  while (head) {
+    to_remove = head;
+    head = head->next;
+    delete to_remove;
+  }
+  tail = nullptr;
+  len = 0;
+}
+
+
+void DLL::remove(int d) {
+  if (head && head->data == d) {
+    pop_front();
+  } else {
+    Node* iter = head;
+    while (iter->next) {
+      if (iter->next->data == d) {
+        Node* to_remove = iter->next;
+        iter->next = to_remove->next;
+        iter->next->prev = iter;
+        delete to_remove;
+        --len;
+        break;
+      } else {
+        iter = iter->next;
+      }
+    }
+  }
+}
+
+void DLL::reverse() {
+  if (len <= 1) return;
+  Node* prev = nullptr;
+  Node* curr = head;
+  Node* next;
+  while (curr) {
+    next = curr->next;
+    curr->next = prev;
+    curr->prev = next;
+    prev = curr;
+    curr = next;
+  }
+  tail = head;
+  head = prev;
+}
+
+void DLL::print(std::ostream& oss) {
+  Node* temp = head;
+  while (temp) {
+    oss << temp->data << ", ";
+    temp = temp->next;
+  }
+  oss << std::endl;
+  return;
+}
---
+++ source/Linear/SLL.cpp
@@ -0,0 +1,208 @@
+#include "SLL.hpp"
+
+SLL::SLL() {
+  head = nullptr;
+  len = 0;
+}
+
+SLL::~SLL() {
+  clear();
+}
+
+unsigned int SLL::length() {
+  return len;
+}
+
+void SLL::push_front(int d) {
+  Node* n = new Node(d);
+  if (!head) {
+    head = n;
+  } else {
+    n->next = head;
+    head = n;
+  }
+  ++len;
+}
+
+int SLL::pop_front() {
+  Node* to_remove = head;
+  int rval = head->data;
+  if (len == 1) {
+    head = nullptr;
+  } else {
+    head = to_remove->next;
+  }
+  delete to_remove;
+  --len;
+  return rval;
+}
+
+int SLL::index(int data) {
+  Node* temp = head;
+  int i = 0;
+  while (temp) {
+    if (temp->data == data)
+      return i;
+    i += 1;
+    temp = temp->next;
+  }
+  return -1;
+}
+
+void SLL::push_back(int d) {
+  Node* n = new Node(d);
+  if (!head) {
+    head = n;
+  } else {
+    Node* iter = head;
+    while (iter->next) {
+      iter = iter->next;
+    }
+    iter->next = n;
+  }
+  ++len;
+}
+
+int SLL::pop_back() {
+  Node* to_remove;
+  if (len == 1) {
+    to_remove = head;
+    head = nullptr;
+  } else {
+    Node* iter = head;
+    while (iter->next->next) {
+      iter = iter->next;
+    }
+    to_remove = iter->next;
+    iter->next = nullptr;
+  }
+  int rval = to_remove->data;
+  delete to_remove;
+  --len;
+  return rval;
+}
+
+int SLL::at(int idx) {
+  // Handle negative indexes by making it positive.
+  idx = (idx < 0) ? len + idx : idx;
+
+  // Check for out of bounds.
+  if (idx < 0 || (unsigned int)idx >= len)
+    throw "Bad index";
+
+  // Iterate to the element.
+  Node* iter = head;
+  for (int i = 0; i < idx; ++i) {
+    iter = iter->next;
+  }
+
+  // Return it's data.
+  return iter->data;
+}
+
+int SLL::set(int idx, int data) {
+  // Handle negative indexes by making it positive.
+  idx = (idx < 0) ? len + idx : idx;
+
+  // Check for out of bounds.
+  if (idx < 0 || (unsigned int)idx >= len)
+    throw "Bad index";
+
+  Node* iter = head;
+  while (--idx >= 0) {
+    iter = iter->next;
+  }
+  int old = iter->data;
+  iter->data = data;
+  return old;
+}
+
+void SLL::push(int d, int idx) {
+  if (!idx) {
+    push_front(d);
+  } else if ((unsigned int)idx == len) {
+    push_back(d);
+  } else {
+    Node* n = new Node(d);
+    Node* iter = head;
+    while (--idx > 0) {
+      iter = iter->next;
+    }
+    n->next = iter->next;
+    iter->next = n;
+    ++len;
+  }
+}
+
+int SLL::pop(int idx) {
+  if (!idx) {
+    return pop_front();
+  } else if ((unsigned int)idx == len - 1) {
+    return pop_back();
+  } else {
+    Node* iter = head;
+    while (--idx > 0) {
+      iter = iter->next;
+    }
+    Node* to_remove = iter->next;
+    iter->next = to_remove->next;
+    int ret = to_remove->data;
+    delete to_remove;
+    --len;
+    return ret;
+  }
+}
+
+void SLL::clear() {
+  Node* to_remove;
+  while (head) {
+    to_remove = head;
+    head = head->next;
+    delete to_remove;
+  }
+  len = 0;
+}
+
+void SLL::remove(int d) {
+  if (head && head->data == d)
+    pop_front();
+  else {
+    Node* iter = head;
+    while (iter->next) {
+      if (iter->next->data == d) {
+        Node* to_remove = iter->next;
+        iter->next = to_remove->next;
+        delete to_remove;
+        --len;
+        break;
+      } else {
+        iter = iter->next;
+      }
+    }
+  }
+}
+
+void SLL::reverse() {
+  if (len <= 1)
+    return;
+  Node* prev = nullptr;
+  Node* curr = head;
+  Node* next;
+  while (curr) {
+    next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+  head = prev;
+}
+
+void SLL::print(std::ostream& oss) {
+  Node* temp = head;
+  while (temp) {
+    oss << temp->data << ", ";
+    temp = temp->next;
+  }
+  oss << std::endl;
+  return;
+}
