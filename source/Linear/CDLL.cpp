b99dbac596f4c590938f659783fad68b9dedeef2
Tom <thoward12@outlook.com>
2019-06-20 12:19:21 -0400

    added CDLL

---
+++ source/Linear/CDLL.cpp
@@ -0,0 +1,48 @@
+#include "LinkedList.hpp"
+
+LinkedList::LinkedList() {
+  head = nullptr;
+  len = 0;
+}
+
+LinkedList::~LinkedList() {
+  clear();
+}
+
+unsigned int LinkedList::length() {
+  return len;
+}
+
+void LinkedList::push_front(int d) {}
+
+int LinkedList::pop_front() {}
+
+int LinkedList::index(int data) {}
+
+void LinkedList::push_back(int d) {}
+
+int LinkedList::pop_back() {}
+
+int LinkedList::at(int idx) {}
+
+int LinkedList::set(int idx, int d) {}
+
+void LinkedList::push(int d, int idx) {}
+
+int LinkedList::pop(int idx) {}
+
+void LinkedList::clear() {}
+
+void LinkedList::remove(int d) {}
+
+void LinkedList::reverse() {}
+
+void LinkedList::print(std::ostream& oss) {
+  Node* temp = head;
+  while (temp) {
+    oss << temp->data << ", ";
+    temp = temp->next;
+  }
+  oss << std::endl;
+  return;
+}
