a3525deab4476131843f9466935af5963cc135b5
Tom <thoward12@outlook.com>
2019-06-25 14:38:38 -0400

    git update from staging

--- source/Linear/Stack.hpp
+++ source/Linear/Stack.hpp
@@ -8,9 +8,39 @@
  */
 class Stack {
  private:
-<<<<<<< HEAD
-=======
   LinkedList* list;
+
+ public:
+  Stack();
+  ~Stack();
+
+  /** size()
+   * Returns the size of the stack.
+   */
+  unsigned int size();
+
+  /** push(int data)
+   * Pushes a node containing the given data to the top of the stack.
+   */
+  void push(int data);
+
+  /** pop()
+   * Removes and returns the data at the top of the stack.
+   */
+  int pop();
+
+  /** top()
+   * Returns the data at the top of the stack without changing the stack.
+   */
+  int top();
+
+  /** empty()
+   * Returns whether or not the stack is empty.
+   */
+  bool empty();
+
+  /** clear()
    * Clears the entire stack.
    */
+  void clear();
 };
