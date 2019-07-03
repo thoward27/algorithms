694617be15c37c7f6b109b7d32b9f74642ac1233
Tom <thoward12@outlook.com>
2019-07-03 12:43:51 -0400

    Passing provided tests, failing added

--- source/Trees/RBTree.cpp
+++ source/Trees/RBTree.cpp
@@ -1,308 +1,286 @@
 #include "RBTree.hpp"
+
 /** Author's Note
  * A large portion of this implementation is derived from Java source code
  * present in Robert Sedgewick's slides on Left-Leaning Red Black Trees,
  * (Avaialable here: https://www.cs.princeton.edu/~rs/talks/LLRB/RedBlack.pdf)
- * that has been translated into C++ and tweaked for the purposes of this course/
+ * that has been translated into C++ and tweaked for the purposes of this
+ * course/
  */
-
-/** Node Definitions */
-
-/** Default Constructor */
-RBNode::RBNode() {
-    color = 0;
-    key = 0;
-    left = nullptr;
-    right = nullptr;
-}
-
-
-/** Destructor */
-RBNode::~RBNode() {
-    //Nothing allocated dynamically, nothing to deallocate
-}
-
-
-/** Initialized Constructor */
-RBNode::RBNode(int inkey,
-               bool incolor,  
-               RBNode * inleft, 
-               RBNode * inright) {
-
-    key = inkey;
-    color = incolor;
-    left = inleft;
-    right = inright;
-}
-
-
-
-/** Tree Definitions */
-
-/** Default Constructor */
-RBTree::RBTree(){
-    root = nullptr;
-}
-
 
 /** Destructor */
 RBTree::~RBTree() {
-    //do somfin
+  // do somfin
 }
-
 
 /** RotateRight()
  * A method to rotate nodes in a RB tree in order to maintain balance
  */
-RBNode * RBTree::rotateRight(RBNode * node) {
-    RBNode * temp = node->left;
-    node->left = temp->right;
-    temp->right = node;
-    temp->color = temp->right->color;
-    temp->right->color = 1;
-    if(node == root) root = temp;
-    return temp;
+Node* RBTree::rotateRight(Node* node) {
+  Node* temp = node->left;
+  node->left = temp->right;
+  temp->right = node;
+  temp->color = temp->right->color;
+  temp->right->color = 1;
+  if (node == root)
+    root = temp;
+  return temp;
 }
-
 
 /** RotateLeft()
  * A method to rotate nodes in a RB tree in order to maintain balance
  */
-RBNode * RBTree::rotateLeft(RBNode * node) {
-    RBNode * temp = node->right;
-    node->right = temp->left;
-    temp->left = node;
-    temp->color = temp->left->color;
-    temp->left->color = 1;
-    if(node == root) root = temp;
-    return temp;
+Node* RBTree::rotateLeft(Node* node) {
+  Node* temp = node->right;
+  node->right = temp->left;
+  temp->left = node;
+  temp->color = temp->left->color;
+  temp->left->color = 1;
+  if (node == root)
+    root = temp;
+  return temp;
 }
-
 
 /** FlipColors()
  * A method to flip the colors of a nodes links in order to maintain balance
  */
-void RBTree::flipColors(RBNode * node) {
-    node->color = !node->color;
-    node->left->color = !node->left->color;
-    node->right->color = !node->right->color;
+void RBTree::flipColors(Node* node) {
+  node->color = !node->color;
+  node->left->color = !node->left->color;
+  node->right->color = !node->right->color;
 }
-
 
 /** Insert()
- * A method to insert keys into the trees 
+ * A method to insert keys into the trees
  */
-RBNode * RBTree::insert(int keyvalue, RBNode * temp) {
-    //if at bottom insert node
-    if(!root) {
-        RBNode * insertnode = new RBNode(keyvalue, 0);
-        root = insertnode;
-        return insertnode;
-    }
+Node* RBTree::insert(int data, Node* temp) {
+  // if at bottom insert node
+  if (!root) {
+    Node* insertnode = new Node(data, 0);
+    root = insertnode;
+    return insertnode;
+  }
 
-    if(!temp) {
-        RBNode * insertnode = new RBNode(keyvalue, 1);
-        temp = insertnode;
-        return insertnode;
-    }
+  if (!temp) {
+    Node* insertnode = new Node(data, 1);
+    temp = insertnode;
+    return insertnode;
+  }
 
-    //recurse to bottom
-    if(keyvalue < temp->key) {
-        temp->left = insert(keyvalue, temp->left);
-    }
-    else if(keyvalue > temp->key) {
-        temp->right = insert(keyvalue, temp->right);
-    }
+  // recurse to bottom
+  if (data < temp->data) {
+    temp->left = insert(data, temp->left);
+  } else if (data > temp->data) {
+    temp->right = insert(data, temp->right);
+  }
 
-    temp = fix(temp);
-    
-    return temp;
+  temp = fix(temp);
+
+  return temp;
 }
-
 
 /** Search()
  * A method to search a RBTree for a given key
  */
-bool RBTree::search(int deskey) {
-    RBNode * trav = root;
-    for( ; trav != nullptr ; ) {
-        if(trav->key == deskey) {
-            return true;
-        }
-        else if(trav->key > deskey) {
-            trav = trav->left;
-        }
-        else if(trav->key < deskey) {
-            trav = trav->right;
-        }
-    }
+bool RBTree::search(int data, Node* n) {
+  if (!n)
     return false;
+  else if (data == n->data)
+    return true;
+  else if (data < n->data)
+    return search(data, n->left);
+  else
+    return search(data, n->right);
 }
-
 
 /** Fix()
  * A method to fix the tree
  */
-RBNode * RBTree::fix(RBNode * temp) {
-    if(temp->right != nullptr && temp->right->color) {
-        temp = rotateLeft(temp);
-    }
-    
-    if(temp->left != nullptr && temp->left->color) {
-        temp = rotateRight(temp);
-    }
+Node* RBTree::fix(Node* temp) {
+  if (temp->right != nullptr && temp->right->color) {
+    temp = rotateLeft(temp);
+  }
 
-    if(temp->left && temp->right) {
-        if(temp->left->color && temp->right->color) {
-            flipColors(temp);
-        }
-    }
+  if (temp->left != nullptr && temp->left->color) {
+    temp = rotateRight(temp);
+  }
 
-    return temp;
+  if (temp->left && temp->right) {
+    if (temp->left->color && temp->right->color) {
+      flipColors(temp);
+    }
+  }
+
+  return temp;
 }
-
 
 /** moveRedLeft()
  * A method to aid in deleting a key
  */
-RBNode * RBTree::moveRedLeft(RBNode * temp) {
+Node* RBTree::moveRedLeft(Node* temp) {
+  flipColors(temp);
+  if (temp->right->left != nullptr && temp->right->left->color) {
+    temp->right = rotateRight(temp->right);
+    temp = rotateLeft(temp);
     flipColors(temp);
-    if(temp->right->left != nullptr && temp->right->left->color) {
-        temp->right = rotateRight(temp->right);
-        temp = rotateLeft(temp);
-        flipColors(temp);
-    }
-    return temp;
+  }
+  return temp;
 }
-
 
 /** moveRedRight()
  * A method to aid in deleting a key
  */
-RBNode * RBTree::moveRedRight(RBNode * temp) {
+Node* RBTree::moveRedRight(Node* temp) {
+  flipColors(temp);
+  if (temp->left->left != nullptr && temp->left->left->color) {
+    temp = rotateRight(temp);
     flipColors(temp);
-    if(temp->left->left != nullptr && temp->left->left->color) {
-        temp = rotateRight(temp);
-        flipColors(temp);
-    }
-    return temp;
+  }
+  return temp;
 }
-
 
 /** Delete the tree form the root */
 void RBTree::deleteMax() {
-    root = deleteMax(root);
-    root->color = 0;
+  root = deleteMax(root);
+  root->color = 0;
 }
-
 
 /** DeleteMax()
  * A method to aid in deleteing a key
  */
-RBNode * RBTree::deleteMax(RBNode * temp) {
-    if(temp->left != nullptr && temp->left->color) {
-        temp = rotateRight(temp);
-    }
+Node* RBTree::deleteMax(Node* temp) {
+  if (temp->left != nullptr && temp->left->color) {
+    temp = rotateRight(temp);
+  }
 
-    if(temp->right == nullptr) {
-        delete temp;
-        return nullptr;
-    }
+  if (temp->right == nullptr) {
+    delete temp;
+    return nullptr;
+  }
 
-    if(temp->right != nullptr && temp->right->color) {
-        temp = moveRedRight(temp->left);
-    }
-    return fix(temp);
+  if (temp->right != nullptr && temp->right->color) {
+    temp = moveRedRight(temp->left);
+  }
+  return fix(temp);
 }
-
 
 /** Delete the tree form the root */
 void RBTree::deleteMin() {
-    root = deleteMin(root);
-    root->color = 0;
+  root = deleteMin(root);
+  root->color = 0;
 }
-
 
 /** DeleteMax()
  * A method to aid in deleteing a key
  */
-RBNode * RBTree::deleteMin(RBNode * temp) {
-    if(temp->left == nullptr) {
-        delete temp;
-        return nullptr;
-    }
+Node* RBTree::deleteMin(Node* temp) {
+  if (temp->left == nullptr) {
+    delete temp;
+    return nullptr;
+  }
 
-    if(temp->left != nullptr && temp->left->color && 
-       temp->left->left != nullptr && temp->left->left->color) {
-        temp = moveRedLeft(temp);
-    }
+  if (temp->left != nullptr && temp->left->color &&
+      temp->left->left != nullptr && temp->left->left->color) {
+    temp = moveRedLeft(temp);
+  }
 
-    temp->left = deleteMin(temp->left);
-    return fix(temp);
+  temp->left = deleteMin(temp->left);
+  return fix(temp);
 }
-
 
 /** Min()
  * A method to find the successor to a node, aids in deletion
  */
-int RBTree::min(RBNode * subtree, bool first = 1) {
-    if(first) return min(subtree->right, 0);
-    if(subtree->left == nullptr) return subtree->key;
-    return min(subtree->left);
+int RBTree::min(Node* subtree, bool first = 1) {
+  if (first)
+    return min(subtree->right, 0);
+  if (subtree->left == nullptr)
+    return subtree->data;
+  return min(subtree->left);
 }
 
-
-/** Deletekey() 
+/** Deletekey()
  * Delete a key in the tree with the given key
  */
-RBNode * RBTree::deletekey(RBNode * temp, int delkey) {
-   
-    int comparison;
-    if(temp->key == delkey) {
-        comparison = 0;
+Node* RBTree::remove(int data, Node* temp) {
+  int comparison;
+  if (temp->data == data) {
+    comparison = 0;
+  } else if (temp->data < data) {
+    comparison = 1;
+  } else {
+    comparison = -1;
+  }
+
+  if (comparison < 0) {
+    if (temp->left != nullptr && temp->left->color &&
+        temp->left->left != nullptr && temp->left->left->color) {
+      temp = moveRedLeft(temp);
     }
-    else if(temp->key < delkey) {
-        comparison = 1;
-    }
-    else {
-        comparison = -1;
+    temp->left = remove(data, temp->left);
+  }
+
+  else {
+    if (temp->left && temp->left->color)
+      temp = rotateRight(temp);
+
+    if (comparison == 0 && temp->right == nullptr) {
+      return nullptr;
     }
 
-    if(comparison < 0) {
-        if(temp->left != nullptr && temp->left->color && 
-           temp->left->left != nullptr && temp->left->left->color) {
-            temp = moveRedLeft(temp);
-        }
-        temp->left = deletekey(temp->left, delkey);
+    if (temp->right != nullptr && temp->right->color &&
+        temp->right->left != nullptr && temp->right->left->color) {
+      temp = moveRedRight(temp);
     }
-    
-    else {
-        if(temp->left && temp->left->color) temp = rotateRight(temp);
 
-        if(comparison == 0 && temp->right == nullptr) {
-            return nullptr;
-        }
-
-        if(temp->right != nullptr && temp->right->color && 
-           temp->right->left != nullptr && temp->right->left->color) {
-            temp = moveRedRight(temp);
-        }
-
-        if(comparison == 0) {
-            temp->key = min(temp->right);
-            temp->right = deleteMin(temp->right);
-        }
-        else {
-            temp->right = deletekey(temp->right, delkey);
-        }
+    if (comparison == 0) {
+      temp->data = min(temp->right);
+      temp->right = deleteMin(temp->right);
+    } else {
+      temp->right = remove(data, temp->right);
     }
-    return fix(temp);
+  }
+  return fix(temp);
 }
 
+void RBTree::preorder(std::ostream& oss) {
+  preorder(root, oss);
+  oss << std::endl;
+}
 
-/** getRoot()
- * Getter for root data member
- */
-RBNode * RBTree::getRoot() {
-    return root;
+void RBTree::preorder(Node* n, std::ostream& oss) {
+  if (!n)
+    return;
+  oss << n->data << ", ";
+
+  preorder(n->left, oss);
+  preorder(n->right, oss);
+}
+
+void RBTree::inorder(Node* n, std::ostream& oss) {
+  if (!n)
+    return;
+  inorder(n->left, oss);
+
+  oss << n->data << ", ";
+
+  inorder(n->right, oss);
+}
+
+void RBTree::inorder(std::ostream& oss) {
+  inorder(root, oss);
+  oss << std::endl;
+}
+
+void RBTree::postorder(Node* n, std::ostream& oss) {
+  if (!n)
+    return;
+  postorder(n->left, oss);
+  postorder(n->right, oss);
+  oss << n->data << ", ";
+}
+
+void RBTree::postorder(std::ostream& oss) {
+  postorder(root, oss);
+  oss << std::endl;
 }
--- source/Trees/RBTree.test.cpp
+++ source/Trees/RBTree.test.cpp
@@ -4,87 +4,81 @@
 #include "RBTree.hpp"
 
 TEST_CASE("Constructor") {
-    RBTree test;
-    CHECK(test.getRoot() == nullptr);
-}
-
-TEST_CASE("Get Root Method") {
-    RBTree test;
-    SUBCASE("No Root"){
-        REQUIRE(test.getRoot() == nullptr);
-    }
-
-    SUBCASE("Initial Root") {
-        test.insert(1, test.getRoot());
-        REQUIRE(test.getRoot() != nullptr);
-    }
+  RBTree tree;
+  REQUIRE(tree.is_empty());
 }
 
 TEST_CASE("Insert Method") {
-    RBTree test;
-    SUBCASE("To Root") {
-        test.insert(3, test.getRoot());
-        REQUIRE(test.getRoot() != nullptr);
-        REQUIRE(test.search(3) == 1);
+  RBTree tree;
+  SUBCASE("To Root") {
+    tree.insert(3);
+    REQUIRE_FALSE(tree.is_empty());
+    REQUIRE(tree.search(3));
+  }
+  SUBCASE("Shifting") {
+    for (int i = 0; i < 3; ++i)
+      tree.insert(i);
+    // std::ostringstream oss;
+    // tree.preorder(oss);
+    // REQUIRE_EQ(oss.str(), "1, 0, 2, \n");
+  }
+  SUBCASE("Beyond Root: Linear Insertions") {
+    for (int i = 0; i < 10; ++i) {
+      tree.insert(i);
+      REQUIRE(tree.search(i) == 1);
     }
-    SUBCASE("Beyond Root: Linear Insertions") {
-        for(int i = 0 ; i < 10 ; ++i) {
-            test.insert(i, test.getRoot());
-            REQUIRE(test.search(i) == 1);
-        }
+  }
+  SUBCASE("Beyond Root: Large Linear Insertions") {
+    for (int i = 0; i < 100; ++i) {
+      tree.insert(i);
+      REQUIRE(tree.search(i) == 1);
     }
-    SUBCASE("Beyond Root: Large Linear Insertions") {
-        for(int i = 0 ; i < 100 ; ++i) {
-            test.insert(i, test.getRoot());
-            REQUIRE(test.search(i) == 1);
-        }
+  }
+  SUBCASE("Beyond Root: Reverse Linear Insertions") {
+    for (int i = 10; i > 0; --i) {
+      tree.insert(i);
+      REQUIRE(tree.search(i) == 1);
     }
-    SUBCASE("Beyond Root: Reverse Linear Insertions") {
-        for(int i = 10 ; i > 0 ; --i) {
-            test.insert(i, test.getRoot());
-            REQUIRE(test.search(i) == 1);
-        }
-        
-    }
-    SUBCASE("Beyond Root: Random Insertions") {
-        test.insert(5, test.getRoot()); 
-        test.insert(2, test.getRoot()); 
-        test.insert(6, test.getRoot()); 
-        test.insert(4, test.getRoot()); 
-        test.insert(3, test.getRoot()); 
-        test.insert(7, test.getRoot()); 
-        test.insert(0, test.getRoot()); 
-        REQUIRE(test.search(3) == 1);
-        REQUIRE(test.search(0) == 1);
-        REQUIRE(test.search(5) == 1);
-        REQUIRE(test.search(6) == 1);
-        REQUIRE(test.search(4) == 1);
-    }
+  }
+  SUBCASE("Beyond Root: Random Insertions") {
+    tree.insert(5);
+    tree.insert(2);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(0);
+    REQUIRE(tree.search(3) == 1);
+    REQUIRE(tree.search(0) == 1);
+    REQUIRE(tree.search(5) == 1);
+    REQUIRE(tree.search(6) == 1);
+    REQUIRE(tree.search(4) == 1);
+  }
 
-    SUBCASE("Beyond Root: Random Insertions 2") {
-        test.insert(1, test.getRoot()); 
-        test.insert(4, test.getRoot()); 
-        test.insert(2, test.getRoot()); 
-        test.insert(5, test.getRoot()); 
-        test.insert(9, test.getRoot()); 
-        test.insert(0, test.getRoot()); 
-        test.insert(3, test.getRoot()); 
-        test.insert(7, test.getRoot()); 
-        REQUIRE(test.search(3) == 1);
-        REQUIRE(test.search(0) == 1);
-        REQUIRE(test.search(5) == 1);
-        REQUIRE(test.search(1) == 1);
-        REQUIRE(test.search(4) == 1);
-    }
+  SUBCASE("Beyond Root: Random Insertions 2") {
+    tree.insert(1);
+    tree.insert(4);
+    tree.insert(2);
+    tree.insert(5);
+    tree.insert(9);
+    tree.insert(0);
+    tree.insert(3);
+    tree.insert(7);
+    REQUIRE(tree.search(3) == 1);
+    REQUIRE(tree.search(0) == 1);
+    REQUIRE(tree.search(5) == 1);
+    REQUIRE(tree.search(1) == 1);
+    REQUIRE(tree.search(4) == 1);
+  }
 }
 
 TEST_CASE("Delete Method") {
-    RBTree test;
-    for(int i = 0 ; i < 10 ; ++i) {
-        test.insert(i, test.getRoot());
-    }
-    test.deletekey(test.getRoot(), 2);
-    REQUIRE(test.search(2) == 0);
-    REQUIRE(test.search(0) == 1);
-    REQUIRE(test.search(4) == 1);
+  RBTree tree;
+  for (int i = 0; i < 10; ++i) {
+    tree.insert(i);
+  }
+  tree.remove(2);
+  REQUIRE(tree.search(2) == 0);
+  REQUIRE(tree.search(0) == 1);
+  REQUIRE(tree.search(4) == 1);
 }
